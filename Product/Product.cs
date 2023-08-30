5f6dcec4aef5f620820ed2e37da28ae33684b246
SHUBH <SHUBH@SHUBH>
2023-08-30 19:42:42 +0530

    Add project files.

---
+++ Product/Product.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    public class Product
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int price { get ; set; }
+        public string Company { get; set; }
+
+    }
+
+    public class ProductCRUD
+    {
+        private List<Product> productlist;
+        public ProductCRUD()
+        {
+            productlist = new List<Product>()
+            {
+                new Product() {id=1,name="laptop",price=50000,Company="HP"},
+                new Product() {id = 2, name = "PC", price = 60000, Company = "ASUS"},
+                new Product() {id = 3,name="Moniter",price=30000,Company="LG"}
+            };
+        }
+
+        /*--------------------------------//display list-------------------------------*/
+        public List<Product> GetProduct()
+        {
+            return productlist;
+        }
+
+        /*-----------------------Grt product by id---------------------------------------*/
+        public Product GetProductByid(int id)
+        {
+            Product product = new Product();
+            foreach (Product p in productlist)
+            {
+                if (p.id == id)
+                {
+                    product = p; 
+                    break;
+                }
+            }
+            return product;
+        }
+        /*------------------------------------ADD Product ------------------------------*/
+        public void ADDProduct(Product P)
+        {
+            productlist.Add(P);
+
+        }
+
+        /*-------------------------------------Update Product------------------------------------------------*/
+        public void UpdateProduct(Product P)
+        {
+            foreach (Product item in productlist)
+            {
+                if(item.id == P.id)
+                {
+                    item.name = P.name;
+                    item.price = P.price;
+                    item.Company = P.Company;
+                    break;
+                }
+            }
+        }
+        /*------------------------------Delete Product---------------------------*/
+        public void DeletProduct(int id)
+        {
+            foreach(Product item in productlist)
+            {
+                if (item.id == id)
+                {
+                    productlist.Remove(item);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Product/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int no = 0;
+            ProductCRUD crud = new ProductCRUD();
+            do
+            {
+                Console.WriteLine("--------------");
+                Console.WriteLine("1.Product List");
+                Console.WriteLine("2.Get Product By ID");
+                Console.WriteLine("3.Add Product");
+                Console.WriteLine("4.Update Product");
+                Console.WriteLine("5.Delete Product");
+                Console.WriteLine("Select Your Option");
+                int Option = Convert.ToInt32(Console.ReadLine());
+
+                switch (Option)
+                {
+                    /*--------------------------------//display list-------------------------------*/
+                    case 1:
+                        List<Product> list = crud.GetProduct();
+                        Console.WriteLine("id\tName\tPrice\tCompany");
+                        foreach (Product product in list)
+                        {
+                            Console.WriteLine($"{product.id}\t{product.name}" +
+                                $"\t{product.price}\t{product.Company}");
+                        }
+                        break;
+
+                    /*----------------------------search product---------------------------*/
+                    case 2:
+                        Console.WriteLine("Enter The Product Id : ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        Product p = crud.GetProductByid(id);
+                        Console.WriteLine("id\tName\tPrice\tCompany");
+                        Console.WriteLine($"{p.id}\t{p.name}\t{p.price}\t{p.Company}");
+                        break;
+                    /*------------------------------------ADD Product------------------------------------*/
+                    case 3:
+                        Product p1 = new Product();
+                        Console.WriteLine("Enter Product Id :");
+                        p1.id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enetr Product Name :");
+                        p1.name = Console.ReadLine();
+                        Console.WriteLine("Enter Price :");
+                        p1.price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Company :");
+                        p1.Company = Console.ReadLine();
+                        crud.ADDProduct(p1);
+                        Console.WriteLine("Product Updated");
+                        break;
+
+                    case 4:
+                        Product p2 = new Product();
+                        Console.WriteLine("Enter Product Id :");
+                        p2.id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enetr Product Name :");
+                        p2.name = Console.ReadLine();
+                        Console.WriteLine("Enter Price :");
+                        p2.price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Company :");
+                        p2.Company = Console.ReadLine();
+                        crud.UpdateProduct(p2);
+                        Console.WriteLine("Product Updated ..");
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Enter the Product id");
+                        int id2 = Convert.ToInt32(Console.ReadLine());
+                        crud.DeletProduct(id2);
+                        Console.WriteLine($"{id2}Product Deleted !!!!!");
+                        break;
+
+                }
+                Console.WriteLine("Press 1 for continue ");
+                no = Convert.ToInt32(Console.ReadLine());
+            }
+            while (no == 1);
+        }
+    }
+]]}
